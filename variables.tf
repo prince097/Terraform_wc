cc0b0a3318759eb30cbbae1d5cd7eda8a7ca5272
Prince <53857267+prince097@users.noreply.github.com>
2024-02-28 13:58:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "AWS_REGION" {
 
 variable "AMI" {
     type = map(string)
-  # By using map we can provide multiple region and it will use this type map for matching the region mentioned to ami id. 
+  # By using map we can provide multiple values in this case regions in terms of key pairs and it will use this type map for matching the region mentioned to ami id. 
     default = {
         ap-south-1 = "ami-03f4878755434977f"
     }
