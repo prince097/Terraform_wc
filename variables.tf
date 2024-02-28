3dfe8a156fb5c7ee512dc3cfb6f0bd6832d03463
Prince <53857267+prince097@users.noreply.github.com>
2024-02-28 13:41:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,12 @@
 variable "AWS_REGION" {
-    default = "us-west-2"
+    default = "ap-south-1"
 }
 
 variable "AMI" {
     type = map(string)
 
     default = {
-        us-west-2 = "ami-0d593311db5abb72b"
-        us-east-1 = "ami-0c2a1acae6667e438"
+        ap-south-1 = "ami-03f4878755434977f"
     }
 }
 
